7e06442fa6855c72a32334f7bcd081de711431cb
Luda68 <pril3pskay@yandex.ru>
2024-05-17 16:09:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+if "34">"123":
+    print("OK")
+if "123">"12":
+    print("OK")
+if [1, 2]>[1, 1]:
+    print("OK")
+#if "6"> 5:        #error, разный тип
+    print("OK")
+#if [5, 6] > 5:     #error, разный тип
+    print("OK")
+if "6" != 5:
+    print("OK")
